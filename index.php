0e31e8c5bcb0674af520199eb3ed4113f7f2a295
simonvherzele <simon.vanherzele@gmail.com>
2017-04-26 15:17:00 +0200

    added index

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+	
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Kv Mechelen</title>
+</head>
+<body>
+	<div id="container">
+
+		<?php include_once("header.inc.php") ?>
+
+		<section id="content">
+			
+		</section>
+
+		<?php include_once("nav.inc.php") ?>
+		
+	</div>
+</body>
+</html>
